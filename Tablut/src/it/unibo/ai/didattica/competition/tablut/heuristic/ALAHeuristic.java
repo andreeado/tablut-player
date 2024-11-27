bf027ccedb1556a16eb99cd6b55c8ed4538ebf27
26leonardo <billi.leonardo@gmail.com>
2024-11-27 15:14:38 +0100

    void heuristic

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/heuristic/ALAHeuristic.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/heuristic/ALAHeuristic.java
@@ -2,6 +2,6 @@ package it.unibo.ai.didattica.competition.tablut.heuristic;
 
 import it.unibo.ai.didattica.competition.tablut.domain.State;
 
-public interface Heuristic {
+public interface ALAHeuristic {
     // double getValue(State state);
 }
--- Tablut/src/it/unibo/ai/didattica/competition/tablut/heuristic/ALAWhiteHeuristic.java
+++
@@ -1,6 +0,0 @@
-package it.unibo.ai.didattica.competition.tablut.heuristic;
-
-import it.unibo.ai.didattica.competition.tablut.domain.State;
-
-public class ALAWhiteHeuristic implements Heuristic{
-}
