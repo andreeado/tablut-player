01ab269cf347500cd8972825175f560bf8319e36
26leonardo <billi.leonardo@gmail.com>
2024-11-28 22:55:07 +0100

    pknmevofvn or

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/heuristic/ALAHeuristic.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/heuristic/ALAHeuristic.java
@@ -272,20 +272,21 @@ public class ALAHeuristic {
     }
 
     public float evaluate(State state) {
-        // int[] kingPos = getKingPosition(state);
-        // Pawn[][] board = state.getBoard();
+        int[] kingPos = getKingPosition(state);
+        Pawn[][] board = state.getBoard();
         int whitePieces = state.getNumberOf(Pawn.WHITE);
         int blackPieces= state.getNumberOf(Pawn.BLACK);
-        // float euristic;
-        // try {
-        //     H5NeuralNetworkPredictor NN = new H5NeuralNetworkPredictor("model.h5");
-        //     euristic = NN.predict(calculatePieceDifference(whitePieces, blackPieces), calculateKingEscapeDistance(kingPos), calculateDangerMetric(kingPos, board), findAvailableEscapes(kingPos, board), findFreePaths(kingPos, board));
-        // } catch (IOException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        //     euristic = 0;
-        // }
+        float euristic;
+        try {
+            H5NeuralNetworkPredictor NN = new H5NeuralNetworkPredictor("C:\\Users\\billi\\Desktop\\TABLOOT\\n" + //
+                                "ostro\\tablut-player\\Tablut\\src\\it\\unibo\\ai\\didattica\\competition\\tablut\\heuristic\\model.h5");
+            euristic = NN.predict(calculatePieceDifference(whitePieces, blackPieces), calculateKingEscapeDistance(kingPos), calculateDangerMetric(kingPos, board), findAvailableEscapes(kingPos, board), findFreePaths(kingPos, board));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            euristic = 0.0f;
+        }
         // ritorna il valore della rete neurale che prende in input le funzioni private con parametri this.
-        return whitePieces - blackPieces;
+        return euristic;
     }
 }
